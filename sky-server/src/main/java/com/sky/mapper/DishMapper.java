b2d33f6be0891477e29c1e46e6be79307674aa03
ppxiaoxu <hello@ppxiaoxu.cn>
2025-09-05 19:08:15 +0800

    新增菜品业务代码开发

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,5 +1,9 @@
 package com.sky.mapper;
 
+import com.sky.annotation.AutoFill;
+import com.sky.entity.Dish;
+import com.sky.enumeration.OperationType;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -7,7 +11,19 @@ import org.apache.ibatis.annotations.Select;
 public interface DishMapper {
 
 
-
+    /**
+     * 根据分类id查询菜品数量
+     * @param id
+     * @return
+     */
     @Select("select count(id) from dish where category_id = #{categoryId}")
     Integer countByCategoryId(Long id);
+
+
+    /**
+     * 插入菜品数据
+     * @param dish
+     */
+    @AutoFill(value = OperationType.INSERT)
+    void insert(Dish dish);
 }
