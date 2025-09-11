a4b82789f5e21b0972241e9c54c9fec67f45d040
ppxiaoxu <hello@ppxiaoxu.cn>
2025-09-11 19:58:05 +0800

    商品浏览业务代码开发

---
+++ sky-server/src/main/java/com/sky/service/impl/UserServiceImpl.java
@@ -0,0 +1,78 @@
+package com.sky.service.impl;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.sky.constant.MessageConstant;
+import com.sky.dto.UserLoginDTO;
+import com.sky.entity.User;
+import com.sky.exception.LoginFailedException;
+import com.sky.mapper.UserMapper;
+import com.sky.properties.WeChatProperties;
+import com.sky.service.UserService;
+import com.sky.utils.HttpClientUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Service
+public class UserServiceImpl implements UserService {
+
+    public static final String WX_LOGIN = "https://api.weixin.qq.com/sns/jscode2session";
+
+    @Autowired
+    private WeChatProperties properties;
+
+    @Autowired
+    private UserMapper userMapper;
+    /**
+     * 微信登录
+     * @param userLoginDTO
+     * @return
+     */
+    public User wxLogin(UserLoginDTO userLoginDTO) {
+        log.info("微信登录，参数：{}", userLoginDTO);
+        String openid = getOpenid(userLoginDTO.getCode());
+        //判断openid是否为空，如果为空，登录失败，抛出业务异常
+        if (openid == null) {
+            throw new LoginFailedException(MessageConstant.LOGIN_FAILED);
+        }
+        //判断当前用户是否为新用户
+        User user = userMapper.getByUserId(openid);
+        //是新用户，自动完成注
+        if (user == null) {
+            user = User.builder()
+                    .openid(openid)
+                    .createTime(LocalDateTime.now())
+                    .build();
+            userMapper.insert(user);
+        }
+
+        //返回用户信息
+        return user;
+    }
+
+    /**
+     * 调用微信接口，获取openid
+     * @param code
+     * @return
+     */
+    private String getOpenid(String code) {
+        //调用微信接口服务，获取openid
+        Map<String, String> map = new HashMap<>();
+        map.put("appid", properties.getAppid());
+        map.put("secret", properties.getSecret());
+        map.put("js_code", code);
+        map.put("grant_type", "authorization_code");
+
+        String json = HttpClientUtil.doGet(WX_LOGIN, map);
+        JSONObject jsonObject = JSON.parseObject(json);
+        String openid = jsonObject.getString("openid");
+        return openid;
+    }
+}
